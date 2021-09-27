95051adebdd1655aceb50b2c356903f914e87aad
r3y10 <82143008+r3y10@users.noreply.github.com>
2021-09-27 12:06:51 +0900

    Create Post Migration

---
+++ database/migrations/2021_09_27_024258_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger("id");
+            $table->unsignedBigInteger("user_id");
+            $table->char("name", 255);
+            $table->text("contents");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
